d96666498e0a4a8fded086b86c22ac782924e1f1
ashish98joshi <ashish98joshi@gmail.com>
2024-03-25 18:18:56 +0530

    dashboard card

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,5 +1,5 @@
 export const ActionButton = () => {
-    return <div className="flex gap-2 mx-5 mt-1.5 sm:flex-grow justify-end mr-5">
+    return <div className="flex gap-2 mx-5 mt-1.5 sm:flex-grow justify-end mr-5 sm:pr-2">
         <button className="rounded-full bg-gray-200 w-10 h-10 px-2.5">
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 fill-gray-700" viewBox="0 0 20 20" fill="none">
             <path fillRule="evenodd" d="M18 5v8a2 2 0 01-2 2h-5l-5 4v-4H4a2 2 0 01-2-2V5a2 2 0 012-2h12a2 2 0 012 2zM7 8H5v2h2V8zm2 0h2v2H9V8zm6 0h-2v2h2V8z" clipRule="evenodd" />
